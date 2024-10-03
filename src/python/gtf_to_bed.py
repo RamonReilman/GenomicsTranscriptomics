eb02c4a7bc3fcad790ca5aeb0d1e2c6f7e1e2ba8
rreilman <r.reilman@st.hanze.nl>
2024-10-03 14:41:44 +0200

    Adding new script, to process vcf

--- src/python/gtf_to_bed.py
+++ src/python/gtf_to_bed.py
@@ -1,7 +1,17 @@
-import polars as pl
+"""
+Reads GTF file, extracts gene names, chromosome number, start - stop from exons
+
+Author: Ramon Reilman
+Version: 1
+data: 03-10-2024
+"""
+
+import sys
 import argparse
+import polars as pl
 from gtfparse import read_gtf
-# /students/2024-2025/Thema05/BlaasKanker/etc/gencode.vM35.annotation.gtf
+
+
 def process_cli():
     """
     Parses command line arguments
@@ -23,10 +33,25 @@ def process_cli():
 
 
 def process_input(path):
+    """
+    Read the input file, a GTF file
+
+    PARAMETERS
+    ----------
+    path, a path to the input file
+
+    RETURNS
+    -------
+    df_voor_bed, polars dataframe that contains the bed file data
+    """
+
+
     try:
         gtf_data = read_gtf(path)
         exon_data = gtf_data.filter(pl.col('feature') == 'exon')
         df_voor_bed = exon_data.select(['seqname', 'start', 'end', 'gene_name'])
+
+        # Account for bed index at 0, while gtf starts at 1.
         df_voor_bed = df_voor_bed.with_columns((pl.col("start")-1).alias("start"))
 
         return df_voor_bed
@@ -36,6 +61,14 @@ def process_input(path):
 
 
 def output_file(args, bed_df):
+    """
+    Writes the .bed data to a .bed file
+
+    PARAMETERS
+    ----------
+    args, object that contains users CLI arguments
+    bed_df, polars dataframe that contains the bed file data
+    """
     if args.output_file is None:
         input_file_list = args.input_file.split("/")
         args.output_file = args.input_file.replace(input_file_list[-1], "output.bed")
@@ -43,11 +76,16 @@ def output_file(args, bed_df):
     bed_df.write_csv(args.output_file, separator="\t")
 
 def main():
+
+    """
+    One main to rule them all
+    """
     args = process_cli()
     bed_df = process_input(args.input_file)
     if bed_df is None:
-        return 1
+        sys.exit(1)
     output_file(args, bed_df)
+
 
 
 if __name__ == "__main__":
